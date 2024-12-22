87e8834fc16ada09ba78164928773e428f9fa37b
Alex <alex.devenrgy@gmail.com>
2024-12-22 14:35:25 +0300

    feat: ✅ added footer

---
+++ src/lib/constants.ts
@@ -0,0 +1,139 @@
+export const FOOTER_NAV_ITEMS = [
+	{
+		title: 'Home',
+		href: '/',
+		categories: [
+			{
+				label: 'Features',
+				href: '/features',
+			},
+			{
+				label: 'Blogs',
+				href: '/blogs',
+			},
+			{
+				label: 'Resources',
+				href: '/resources',
+				isNew: true,
+			},
+			{
+				label: 'Testimonials',
+				href: '/testimonials',
+			},
+			{
+				label: 'Contact us',
+				href: '/contacts',
+			},
+			{
+				label: 'Newsletter',
+				href: '/newsletter',
+			},
+		],
+	},
+	{
+		title: 'News',
+		href: '/news',
+		categories: [
+			{
+				label: 'Trending Stories',
+				href: '/trending-stories',
+			},
+			{
+				label: 'Featured Videos',
+				href: '/featured-videos',
+			},
+			{
+				label: 'Techonology',
+				href: '/technology',
+			},
+			{
+				label: 'Health',
+				href: '/health',
+			},
+			{
+				label: 'Politics',
+				href: '/politics',
+			},
+			{
+				label: 'Environment',
+				href: '/environment',
+			},
+		],
+	},
+	{
+		title: 'Blogs',
+		href: 'blogs',
+		categories: [
+			{
+				label: 'Quantum Computing',
+				href: '/quantum-computing',
+			},
+			{
+				label: 'AI Ethics',
+				href: '/ai-ethics',
+			},
+			{
+				label: 'Space Exploration',
+				href: '/space-exploration',
+			},
+			{
+				label: 'Biotechology',
+				href: '/biotechology',
+				isNew: true,
+			},
+			{
+				label: 'Renewable Energy',
+				href: '/renewable-energy',
+			},
+			{
+				label: 'Biohacking',
+				href: '/biohacking',
+			},
+		],
+	},
+	{
+		title: 'Podcasts',
+		href: 'podcasts',
+		categories: [
+			{
+				label: 'AI Revolution',
+				href: '/ai-revolution',
+			},
+			{
+				label: 'AI Revolution',
+				href: '/ai-revolution',
+				isNew: true,
+			},
+			{
+				label: 'TechTalk AI',
+				href: '/tech-talk-ai',
+			},
+			{
+				label: 'AI Conversations',
+				href: '/ai-conversations',
+			},
+		],
+	},
+	{
+		title: 'Resources',
+		href: 'resources',
+		categories: [
+			{
+				label: 'Whitepapers',
+				href: '/whitepapers',
+			},
+			{
+				label: 'Ebooks',
+				href: '/ebooks',
+			},
+			{
+				label: 'Reports',
+				href: '/reports',
+			},
+			{
+				label: 'Research Papers',
+				href: '/research-papers',
+			},
+		],
+	},
+]
